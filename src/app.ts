2c64bf74a92ad5c5660188b353d2a3df8512f3e8
Matheus A. Pereira <matttalves@gmail.com>
2022-09-10 21:49:12 -0300

    feat: setup express app

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import Logging from './library/Logging';
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const logMessage = `Incoming => Method: [${req.method}] - URL: [${req.url}] - IP [${req.socket.remoteAddress}]`;
+  Logging.info(logMessage);
+
+  res.on('finish', () => {
+    Logging.info(logMessage + ` - Status [${res.statusCode}]`);
+  });
+
+  next();
+});
+
+app.get('/ping', (req, res) => res.status(200).json({ message: 'pong' }));
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  Logging.error(error);
+
+  return res.status(404).json({ message: error.message });
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,22 @@
-import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
+import app from './app';
 import { config } from './config/config';
 import Logging from './library/Logging';
 
-const app = express();
+function startServer() {
+  Logging.info('Connected to mongoDB');
+
+  http.createServer(app).listen(config.server.port, () => {
+    Logging.info(`Listening: http://localhost:${config.server.port}`);
+  });
+}
 
 mongoose
   .connect(config.mongo.uri)
-  .then(() => {
-    Logging.info('Connected to mongoDB');
-  })
+  .then(startServer)
   .catch((error) => {
     Logging.error('Unable to connect:');
     Logging.error(error);
+    mongoose.disconnect();
   });
