c877f5fdc9125edf047b81b5945c43bf18e23706
Matheus A. Pereira <matttalves@gmail.com>
2022-09-11 14:36:51 -0300

    feat: author create

--- src/controllers/AuthorController.ts
+++ src/controllers/AuthorController.ts
@@ -1,27 +1,39 @@
 import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
-import AuthorModel from '@models/AuthorModel';
+import AuthorModel, { IAuthor } from '@models/AuthorModel';
 
 class AuthorController {
-  public async create(req: Request, res: Response): Promise<Response> {
-    return res.json({
-      message: 'TODO: Implement CREATE'
-    });
+  public async create(
+    req: Request<{}, {}, IAuthor>,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { name } = req.body;
+
+      const author = await new AuthorModel({ name }).save();
+
+      return res.status(201).json({
+        author
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  public async read(req: Request, res: Response): Promise<Response> {
+  public async read(req: Request, res: Response) {
     return res.json({
       message: 'TODO: Implement READ'
     });
   }
 
-  public async update(req: Request, res: Response): Promise<Response> {
+  public async update(req: Request, res: Response) {
     return res.json({
       message: 'TODO: Implement UPDATE'
     });
   }
 
-  public async delete(req: Request, res: Response): Promise<Response> {
+  public async delete(req: Request, res: Response) {
     return res.json({
       message: 'TODO: Implement DELETE'
     });
