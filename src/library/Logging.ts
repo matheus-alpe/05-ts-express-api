76816f582a34c5d9faf03aba73c3017d4a078c91
Matheus A. Pereira <matttalves@gmail.com>
2022-09-11 14:44:21 -0300

    fix: log library

--- src/library/Logging.ts
+++ src/library/Logging.ts
@@ -2,31 +2,30 @@ import chalk from 'chalk';
 
 export default class Logging {
   public static log(args: any) {
-    this.log(args);
+    const info = `[${new Date().toLocaleString()}] [LOG]`;
+    const message = typeof args === 'string' ? chalk.greenBright(args) : args;
+
+    console.log(chalk.green(info, message));
   }
 
   public static info(args: any) {
-    const info = `[${this.getLocaleDate()}] [INFO]`;
+    const info = `[${new Date().toLocaleString()}] [INFO]`;
     const message = typeof args === 'string' ? chalk.blueBright(args) : args;
 
     console.log(chalk.blue(info, message));
   }
 
   public static warn(args: any) {
-    const warn = `[${this.getLocaleDate()}] [WARN]`;
+    const warn = `[${new Date().toLocaleString()}] [WARN]`;
     const message = typeof args === 'string' ? chalk.yellowBright(args) : args;
 
     console.log(chalk.yellow(warn, message));
   }
 
   public static error(args: any) {
-    const error = `[${this.getLocaleDate()}] [ERROR]`;
+    const error = `[${new Date().toLocaleString()}] [ERROR]`;
     const message = typeof args === 'string' ? chalk.redBright(args) : args;
 
     console.log(chalk.red(error, message));
-  }
-
-  private static getLocaleDate() {
-    return new Date().toLocaleString();
   }
 }
